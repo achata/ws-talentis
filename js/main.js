f19b0ef24a5baad9f7c796d9cefe8749f7172c78
Achata <a.achata.j@gmail.com>
2020-01-24 22:35:32 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$('.box-crr-img').hover(function(){
+    $(this).attr('imgChange','url(https://community-cdn-digitalocean-com.global.ssl.fastly.net/assets/tutorials/images/large/DOM_social01.png?1526575326)');
+});
